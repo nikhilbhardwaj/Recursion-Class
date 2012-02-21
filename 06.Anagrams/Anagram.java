65247b8b5e61309bcbc05c72a1a8324d2ebfc457
deboshrestha <deboshrestha@yahoo.com>
2012-02-21 22:55:33 +0530

    some modifications

--- 06.Anagrams/Anagram.java
+++ 06.Anagrams/Anagram.java
@@ -5,10 +5,48 @@ public class Anagram
    * it should be noted that this is just for understanding the concepts.
    */
 
-  public static void main(String [] args)
-    throws Exception
+
+  private static void RecursivelyPermute(String soFar, String rest)
+  throws Exception
   {
-    EnglishWords dictionary = new EnglishWords();
-    System.out.println(dictionary.hasWord("abacus"));
+    if(rest.equals(""))
+    {
+		EnglishWords dictionary = new EnglishWords();
+		if(dictionary.hasWord(soFar))
+		{
+			System.out.println("The anagram is " +soFar);
+			System.exit(0);
+		}
+
+	}
+	else
+	{
+		for(int i = 0; i < rest.length(); ++i)
+		{
+			String next = soFar + rest.charAt(i);
+			String remaining = rest.substring(0,i) + rest.substring(i+1);
+			RecursivelyPermute(next,remaining);
+		}
+	}
+  }
+
+  //wrapper function to call RecursivelyPermute
+  public static void Permute(String str)
+  throws Exception
+  {
+    RecursivelyPermute("",str);
+  }
+
+  public static void main(String [] args)
+  throws Exception
+  {
+    if( args.length == 0 )
+    {
+      System.err.println("Pass the String you want to check for anagram as the argument to this.");
+    }
+    else
+    {
+      Permute(args[0]);
+    }
   }
 }
